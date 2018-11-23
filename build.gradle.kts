f41f68fe52d2b0f1b42c3daaecaa436bd250da69
Jeff Hudren <hudren@gmail.com>
2018-11-23 10:57:00 -0700

    Cleaned up the dependency versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,17 @@
-import org.gradle.internal.impldep.org.apache.maven.Maven
-import org.jetbrains.kotlin.ir.backend.js.lower.translateEqualsForString
+// Project: kotlin-scripting
+
+object Versions {
+    const val kotlinCoroutines = "1.0.1"
+}
 
 group = "io.alphalon.kotlin"
 version = "0.1-SNAPSHOT"
 
 plugins {
-    kotlin("jvm") version "1.3.0"
+    kotlin("jvm") version "1.3.10"
     id("application")
     id("maven-publish")
 }
-
-buildscript {
-    var kotlinCoroutinesVersion: String by extra
-
-    kotlinCoroutinesVersion = "1.0.1"
-}
-
-val kotlinCoroutinesVersion: String by extra
 
 application {
     mainClassName = "io.alphalon.kotlin.scripting.MainKt"
@@ -34,16 +29,15 @@ repositories {
 dependencies {
     compile(kotlin("stdlib-jdk8"))
     compile(kotlin("scripting-jvm"))
-    compile("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:$kotlinCoroutinesVersion")
+    compile("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:${Versions.kotlinCoroutines}")
 
     // implementation(kotlin("scripting-common"))
     // implementation(kotlin("script-util"))
 
     testCompile(kotlin("test-junit5"))
-    testImplementation("com.github.holgerbrandl:kscript-annotations:1.2")
 }
 
-val jar: Jar by tasks.getting(Jar::class) {
+val jar by tasks.getting(Jar::class) {
     manifest {
         attributes["Implementation-Version"] = version
     }
