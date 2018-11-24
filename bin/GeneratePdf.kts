54eb4bab05645407ef161e9ee52765b6b4966386
Jeff Hudren <hudren@gmail.com>
2018-11-23 19:23:28 -0700

    Wrote more illustrative example

--- bin/GeneratePdf.kts
+++ bin/GeneratePdf.kts
@@ -1,26 +1,41 @@
-// Script: GeneratePdf.kts
+// Project: kotlin-scripting
+// Script:  GeneratePdf.kts
 //
-//DIR $KO_PROJECT
-//CMD GeneratePdf Generates a PDF from the project README.md file
+//DIR  $KO_PROJECT
+//CMD  GeneratePdf Generates a PDF from the project README.md file
 
 import java.io.File
 import java.util.concurrent.TimeUnit
 
+typealias ExitCode = Int
+
 /**
- * Executes the [commandLine], waiting for the process to finish.
+ * Executes the block on a non-zero exit code and terminates the process.
  */
-fun exec(commandLine: String, workingDir: File? = null): Int {
-    val command = commandLine.split(" ").toTypedArray()
-
-    val process = ProcessBuilder(*command)
-        .directory(workingDir ?: File(System.getProperty("user.dir")))
-        .redirectOutput(ProcessBuilder.Redirect.INHERIT)
-        .redirectError(ProcessBuilder.Redirect.INHERIT)
-        .start()
-
-    process.waitFor(5, TimeUnit.MINUTES)
-
-    return process.exitValue()
+fun ExitCode.fail(block: (() -> Unit)? = null) {
+    if (this > 0) {
+        block?.invoke()
+        System.exit(this)
+    }
 }
 
-exec("""pandoc -V geometry:margin=1in README.md -o kotlin-scripting.pdf""")
+/**
+ * Executes the [command], waiting for the process to finish.
+ */
+fun exec(command: List<String>, workingDir: File? = null): ExitCode = ProcessBuilder(command)
+    .directory(workingDir ?: File(System.getProperty("user.dir")))
+    .redirectOutput(ProcessBuilder.Redirect.INHERIT)
+    .redirectError(ProcessBuilder.Redirect.INHERIT)
+    .start().run {
+        waitFor(5, TimeUnit.MINUTES)
+        exitValue()
+    }
+
+/**
+ * Executes the [commandLine], performing a terribly naive parsing operation
+ * to separate the arguments to pass to the process. Use at your own risk!
+ */
+fun exec(commandLine: String) = exec(commandLine.split(" "))
+
+// Use pandoc to generate the PDF file
+exec("pandoc -V geometry:margin=1in README.md -o kotlin-scripting.pdf").fail()
