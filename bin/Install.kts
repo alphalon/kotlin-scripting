ffd045ed5a456cf4fba534e853bca62216e5657b
Jeff Hudren <hudren@gmail.com>
2018-11-29 23:16:57 -0700

    Added local install command

---
+++ bin/Install.kts
@@ -0,0 +1,16 @@
+/*
+ * Project: kotlin-scripting
+ * Script:  install.kts
+ */
+
+//DIR $KO_PROJECT
+//CMD install - Installs the library into the local Maven repo
+
+//COMPILER_OPTS -jvm-target 1.8
+//DEPS io.alphalon.kotlin:kotlin-scripting:0.1.3
+
+import io.alphalon.kotlin.scripting.*
+
+// Build project library and install in local maven repository
+val project: GradleProject = currentProject()
+project.exec("publishToMavenLocal").fail()
